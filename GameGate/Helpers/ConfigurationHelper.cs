7d33f1f05ec80bfa25925d7c0257a3b852a1cf9e
akoc1 <akoclo19@gmail.com>
2025-10-22 16:48:08 +0300

    add configuration helper class to read and write app settings

---
+++ GameGate/Helpers/ConfigurationHelper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Configuration;
+
+namespace GameGate.Helpers
+{
+    public static class ConfigHelper
+    {
+        public static bool FirstTimeOpening => Convert.ToBoolean(Read("FirstTimeOpening"));
+
+        public static object Read(string key)
+        {
+            object value = new object();
+
+            try
+            {
+                value = ConfigurationManager.AppSettings[key];
+            } catch (Exception ex)
+            {
+                // handle exception
+            }
+
+            return value;
+        }
+
+        public static object Write(string key, object value)
+        {
+            try
+            {
+                var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                
+                if (config.AppSettings.Settings[key] != null)
+                    config.AppSettings.Settings[key].Value = value.ToString();
+                else
+                    config.AppSettings.Settings.Add(key, value.ToString());
+
+                config.Save();
+
+                ConfigurationManager.RefreshSection("appSettings");
+            }
+            catch (Exception ex)
+            {
+                // handle exception
+            }
+
+            return value;
+        }
+    }
+}
